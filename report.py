1a5932fd3665206d84fb87957c5d4af22754c113
Айханым <253998@astanait.edu.kz>
2026-05-15 23:45:18 +0500

    Delete report.py

--- report.py
+++
@@ -1,8 +0,0 @@
-class Report:
-        def __init__(self, analyser, saver):
-            self.analyser = analyser
-            self.saver = saver
-
-        def generate(self):
-            print("Генерация отчета...")
-            self.saver.save()
