b09d5efc47de48a2e81f17d89b0e657133658c49
Айханым <253998@astanait.edu.kz>
2026-05-15 23:44:31 +0500

    Delete _init__.py

--- _init__.py
+++
@@ -1,5 +0,0 @@
-from .file_manager import FileManager
-from .data_loader import DataLoader
-from .analyser import SleepAnalyser, GpaAnalyser
-from .result_saver import ResultSaver
-from .report import Report
